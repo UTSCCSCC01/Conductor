e6b5114919cf45f177be3627118cf46a8666c9b6
Ser0n-ath <seron.ath@gmail.com>
2022-07-02 20:34:53 -0400

    CON-75 add way to execute and get name of binaries

---
+++ Cognitavit/src/executor/device_executor/exec.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractclassmethod
+
+class Executor(ABC):
+    
+    @abstractclassmethod
+    def read_native_app_list(self) -> None:
+        ''' Returns a list of apps natively installed apps on this machine, alongside the binary location.
+        '''
+        pass
+    
+    @abstractclassmethod
+    def read_orchestra_app_list(self) -> dict:
+        ''' Returns a list of currently installed bots from the orchestra web store on this machine
+            along side the binary location. The directory is stored in the userapp storage as a json file.
+        '''
+        pass
+
+    @abstractclassmethod
+    def native_exec(self, name:str, params:str, applist:dict) -> None:
+        '''Executes a native application with optional launch params'''
+
+    @abstractclassmethod
+    def bot_exec(self, params:str) ->None:
+        '''Executes a bot application from the orchestra webstore'''
+
+    
---
+++ Cognitavit/src/executor/module_fetcher/module-downloader.py
@@ -0,0 +1,3 @@
+#Code that download from a url and store results in a secret folder.
+#Then creates a shortcut to the binary in the secret folder in a Bot Application folder
+
