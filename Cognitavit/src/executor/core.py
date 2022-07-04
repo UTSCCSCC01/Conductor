7d1cbbc219ed79e5740cce2c30c5faf7fbe495e0
example <email@example.com>
2022-07-03 23:37:26 -0400

    CON-83 complete

--- Cognitavit/src/executor/core.py
+++ Cognitavit/src/executor/core.py
@@ -1,3 +1,4 @@
+import platform
 from time import sleep
 from execFactory import FactoryExecutor
 
@@ -10,8 +11,10 @@ def print_list(list):
 #Create factory executor
 execute_factory = FactoryExecutor()
 #Return an executor object
-platform_exec = execute_factory.create('win')
+platform_name = platform.system()
+platform_exec = execute_factory.create(platform_name) # platform_exec = execute_factory.create('win')
 dev = platform_exec.read_native_app_list()
+
 #  def native_exec(self, name:str, params:str, exec_app_list:Tuple[dict,dict], app_type:int) -> AppStatus:
 
 print_list(dev[0])
@@ -20,44 +23,63 @@ print("End of dev0 list")
 print_list(dev[1])
 print("End of dev1 list")
 
+device_type = platform_name.lower().strip()
+if (device_type == 'win'):
 
 
-#test a system command
-#ps = platform_exec.native_exec("terminal", r" 'shutdown', '/s' ", dev, 0)
-#sleep(3)
 
-#Regular App
-ps = platform_exec.native_exec("chrome", r"https://www.youtube.com/watch?v=dQw4w9WgXcQ", dev, 0)
-sleep(3)
+    #test a system command
+    #ps = platform_exec.native_exec("terminal", r" 'shutdown', '/s' ", dev, 0)
+    #sleep(3)
 
-#Windows Store App
+    #Regular App
+    ps = platform_exec.native_exec("chrome", r"https://www.youtube.com/watch?v=dQw4w9WgXcQ", dev, 0)
+    sleep(3)
 
-ps = platform_exec.native_exec("notepad", r"C:\Users\Seron\Desktop\test.txt", dev, 0)
-sleep(3)
-#Windows Store App
-ps = platform_exec.native_exec("player", r"C:\Users\Seron\Desktop\pro_battles.177886dc.mp3", dev, 0)
-sleep(3)
+    #Windows Store App
 
-#Windows Store App
-ps = platform_exec.native_exec("clock", r"", dev, 0)
-sleep(3)
+    ps = platform_exec.native_exec("notepad", r"C:\Users\Seron\Desktop\test.txt", dev, 0)
+    sleep(3)
+    #Windows Store App
+    ps = platform_exec.native_exec("player", r"C:\Users\Seron\Desktop\pro_battles.177886dc.mp3", dev, 0)
+    sleep(3)
 
-#Installed App
-ps = platform_exec.native_exec("discord", r"", dev, 0)
-sleep(3)
+    #Windows Store App
+    ps = platform_exec.native_exec("clock", r"", dev, 0)
+    sleep(3)
 
-
-#Installed App
-ps = platform_exec.native_exec("edge", r"", dev, 0)
-sleep(3)
-
-#test a system command
-ps = platform_exec.native_exec("terminal", r" 'shutdown', '/s' ", dev, 0)
-sleep(3)
-
-#cancel the restart
-ps = platform_exec.native_exec("terminal", r" 'shutdown', '-a' ", dev, 0)
+    #Installed App
+    ps = platform_exec.native_exec("discord", r"", dev, 0)
+    sleep(3)
 
 
-print(ps)
+    #Installed App
+    ps = platform_exec.native_exec("edge", r"", dev, 0)
+    sleep(3)
 
+    #test a system command
+    ps = platform_exec.native_exec("terminal", r" 'shutdown', '/s' ", dev, 0)
+    sleep(3)
+
+    #cancel the restart
+    ps = platform_exec.native_exec("terminal", r" 'shutdown', '-a' ", dev, 0)
+
+
+    print(ps)
+
+
+
+elif (device_type == 'linux'):
+
+
+
+    ps = platform_exec.native_exec("google-chrome-stable", r" --incognito https://www.youtube.com/watch?v=LhOSM6uCWxk ", dev, 0)
+    sleep(3)
+
+
+
+
+elif (device_type == 'mac'):
+    raise ValueError('not supported')
+else:
+    raise ValueError('unknown platform not supported')
