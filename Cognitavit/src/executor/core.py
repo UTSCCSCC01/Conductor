9e19aaaf5dc6307d05a78ebd665a0ad645baaa20
Ser0n-ath <seron.ath@gmail.com>
2022-07-29 23:50:51 -0400

    -add rest api as ipc sockets -add endpoint for getting native applist -added endpoint for executing applications by name with args

--- Cognitavit/src/executor/core.py
+++ Cognitavit/src/executor/core.py
@@ -1,1 +1,46 @@
-#Removed Demo Code.
+'''
+python-electron executor
+A tool used to read applet list and execute applications based on name or applet id. 
+
+-
+This app lifecycle is managed by electron.
+Upon startup the follow enviroment variables are set.
+
+APPDATA = "path_to_save_data/<userid>/
+ie: the path will look like this  -> C:/path_to_shared_secret/cognitavit/userid/
+
+inside the userid folder we have the following 
+
+Folder content:
+/orchestra_webapps: A folder which consists of shortcuts to the actual binaries. [Located in the download folder] 
+/custom_binaries: A folder which consits of shortcut to bots detail. [Located in random place in drive. (which is why its a shortcut)]
+/downloads: A place where downloaded items are stored.  
+web_app.config: config file of what orchestra web bots are installed.
+
+You may assume that you are already inside the userid/ folder
+
+'''
+
+from flask import Flask, request, jsonify
+
+#Import Routes from network 
+from network.readApplication import read_app as readApplication
+from network.execApplication import exec_app as execApplication
+
+
+# To read the enviroment variables
+import sys
+
+
+port = 0
+if sys.argv.__len__() > 1:
+    port = sys.argv[1]
+
+
+app = Flask(__name__)
+
+app.register_blueprint(readApplication)
+app.register_blueprint(execApplication)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000,debug=True)
---
+++ Cognitavit/src/executor/network/execApplication.py
@@ -0,0 +1,47 @@
+from platform import platform
+from flask import Blueprint, jsonify, request
+from execFactory import FactoryExecutor
+
+exec_app = Blueprint("/execute", __name__)
+sys_platform = 'win'
+
+
+@exec_app.route('/executeApp', methods=['GET', 'POST'])
+def execute_application():
+    '''
+    Executes an application with params
+
+    request_format:
+    {
+        application_name: ...
+        argument: ...
+    }
+
+
+    return format: 
+    json format:
+    {
+        success: True/False
+        date: CURRENT_DATE
+        appname: APPLICATION_NAME
+        argz: ARGUMENTS
+    }
+
+    '''
+    
+    #Do error checking later. 
+    event = request.json
+
+
+    applet_name = event["application_name"]
+    arguments = event["arguments"]
+
+
+    execute_factory = FactoryExecutor()
+    platform_exec = execute_factory.create(sys_platform)
+
+    platform_exec.execute(applet_name, arguments)
+
+
+    return {"status": True}
+    
