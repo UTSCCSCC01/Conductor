58b60f3c65deed8e6cb164532f801b8fd434bda1
Ser0n-ath <seron.ath@gmail.com>
2022-08-04 01:43:15 -0400

    -updated event state schema -added ability to create/delete events

--- Orchestra/backend/eventsystem-service/model/EventOrchestrator.js
+++ Orchestra/backend/eventsystem-service/model/EventOrchestrator.js
@@ -2,24 +2,32 @@ const mongoose = require('mongoose');
 
 // Note: MongoDB automatically creates each document's identifier as "_id".
 const eventExecutionSchema = mongoose.Schema({
-    //Application Lists
-
+    //Given By the user
     EventId: { type: String, required: true}, //EventID stored in the event-builder microservice
-
-
     NumberOfPredicates: {type: Number, required: true}, //predicate microserivce length. 
+    timeToExecute: { type: Date },
+    RunAfterEventPassed: {type: Boolean, required: true},
+    HangtillPredicateSatisfied: {type: Boolean, required: true},
+    ClientFailureRetrySignal: {type: Boolean, required: true},
 
-    TotalPredicateSatisfied: {type: Number, required: true}, //Increment, signal sent by predicate microservice: Initally 0
-
-    PredicateState: {type: Boolean, required: true}, //Finite State switch: switches to true iff NumberOfPredicates == TotalPredicateSatisfied.
-    
-    TimeSatisfied: {type: Boolean, required: true}, // Finite State switch: switches to true iff mongodb stream sends trigger of eventid
+    /*State Variables for event system */
+    TotalPredicateSatisfied: {type: Number, default: 0}, //Increment, signal sent by predicate microservice: Initally 0
+    PredicateState: {type: Boolean, default: false}, //Finite State switch: switches to true iff NumberOfPredicates == TotalPredicateSatisfied.
+    TimeSatisfied: {type: Boolean, default: false}, // Finite State switch: switches to true iff mongodb stream sends trigger of eventid
 
     //Did the dispatcher see this event job, and try to process it
-    DispatcherAcknowledged: {type: Boolean, required: true},
+    DispatcherAcknowledged: {type: Boolean, default: false},
+    //Return Value from the dispatcher: True=Sent, False=Offline
+    UserSemiAcknowledged: {type: Boolean, default: false},
 
-    //Did the dispatcher send this event to the user. 
-    SentToUser: {type: Boolean, required: true},
+
+    //Is the event system done with this event.
+    Terminated: {type: Boolean, default: false},
+
+    
+    ExecutionMessage: {type:String, required: true},
+
+    
 
 });
 
--- Orchestra/backend/eventsystem-service/src/index.js
+++ Orchestra/backend/eventsystem-service/src/index.js
@@ -17,7 +17,7 @@ const mongoURI = MONGO_DB_URI;
 const { EventOrchestrator } = require('../model/EventOrchestrator');
 const { EventClockStream } = require('../model/ClockWork');
 
-console.log("init ClockWorks and EventOrchestrator")
+console.log("Starting ClockWorks and Orchestra")
 
 //Connect to mongodb.
 const connect = mongoose.connect(mongoURI, {
@@ -47,36 +47,326 @@ app.use((err, req, res, next) => {
 });
 
 
-let changeStream;
-async function signalWatcher(){
-    changeStream = EventClockStream.watch();
+/**
+ * The app is structured from top to bottom. Event Node starts at the start and transistions to the bottom where its complete. 
+ */
 
-    changeStream.on("change", next => {
-        console.log("RESULT:", next);
-    })
+/*---------------------------------------- HELPERS TO OBTAIN INFORMATION ABOUT EVENT AND SETTING STATES -------------------------*/
+async function getEventState(event_id){
+    EventOrchestrator.findOne({EventId: event_id}, (error, event_info) => {
+        if (error || event_info == null){
+            return undefined;
+        }
+        return event_info;
+    });
+
+    return undefined;
 }
 
+/*----------------------------------------START OF EVENT DRIVEN FINITE STATE MACHINE LOGIC----------------------------------------*/
+
+
+/**
+ * Adds a event to the EventOrchestrator DB
+ * Adds event to the ClockWorkStream DB
+ */
+
+
+
+
+
+
+
+/*----------------------------------------PREDICATE SIGNAL STATE REGION----------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+/*--------------------------------------- PREDICATE SIGNAL STATE REGION----------------------------------------/*
+
+
+/*----------------------------------------TIME TO EXECUTE SIGNAL STATE REGION----------------------------------------*/
+let changeStream;
+async function signalWatcher(){
+    console.log("Signal watcher has started");
+    changeStream = EventClockStream.watch();
+    changeStream.on("change", next => {
+        //Execution time for an event has passed. 
+
+        
+        if(next.operationType == 'delete'){
+            console.log(next);
+            //handleExecutionDateOccur(next.documentKey._id);
+        }
+    })
+}
 //Start the signal watcher.
 signalWatcher();
 
+//Function on what to do when the execution date has arrived
+async function handleExecutionDateOccur(event_id){
+
+
+}
+//Tells event-builder to send event to dispatcher which sends results to client electron
+// which sends to Trigger over ipc to execute the task.
+async function send_dispatcher(event_id){
+}
+
+async function handle(){
+}
+/*----------------------------------------TIME TO EXECUTE SIGNAL STATE REGION END----------------------------------- */
+
+
+/**
+ * Required: 
+ * {
+ *  eventId:String, 
+ *  TotalNumberOfPredicates:number, 
+ *  DateOfExecution: Date,
+ *  RunAfterEventPassed: boolean
+ *  HangtillPredicateSatisfied: boolean
+ *  ClientFailureRetryEvent: boolean
+ * }
+ * 
+ *  
+ */
+app.post("/event/add", async (req,res) => {
+
+    let input = req.body;
+    console.log("/event/add: Request Body:", req.body);
+
+    let event_id = input.eventId;
+    let total_predicate_count = input.TotalNumberOfPredicates;
+    let date_of_execute = input.DateOfExecution;
+    let run_after_event_passed = input.RunAfterEventPassed;
+    let hang_predicate_satisfied = input.HangtillPredicateSatisfied;
+    let client_failure_retry_send = input.ClientFailureRetryEvent
+
+
+    /**
+     * Adds an event to the orchestra database along side its default values
+     * 
+     * Default Values:
+     *  TotalPredicateSatisfied: 0
+     *  PredicateState: false; true if NumberOfPredicates = true
+     *  TimeSatisfied: false
+     *  DispatcherAcknowledge: false;
+     *  UserSemiAcknowledged: false,
+     *  Terminated: false
+     *  ExecutionMessage: "Waiting for predicate and/or event satisfaction" 
+     */
+
+    let predicate_state = false;
+    if(total_predicate_count == 0){
+        predicate_state = true;
+    }
+    try{
+        let event_payload = {
+            EventId: event_id,
+            NumberOfPredicates: total_predicate_count,
+            timeToExecute: new Date(date_of_execute),
+            RunAfterEventPassed: run_after_event_passed,
+            HangtillPredicateSatisfied: hang_predicate_satisfied,
+            ClientFailureRetrySignal: client_failure_retry_send,
+            //State Default Values:
+            ExecutionMessage: "Waiting for predicate and/or execution signals",
+            PredicateState: predicate_state //Satisfied if no predicates.
+        }
+
+        let events_clockwork = new EventOrchestrator(event_payload);
+        await EventOrchestrator.collection.insertOne(events_clockwork);
+    }catch(e){
+        console.log(e);
+        
+        return res.status(500).send({status: false, msg: "failed to add event to db"});
+    }
+
+    // Add timer to an event. [Add after adding event]
+    let addEventTimer = await add_timer_to_event(event_id, date_of_execute);
+    if(!addEventTimer){
+        return res.status(500).send({status: false, msg: "failed to attach timer to event."});
+    }
+
+    return res.status(200).send({status: true, msg: "added event to database."});
+})
+
+/**
+ *  Add a timer to an event. If the timer has passed, there will be an epsilon transition.
+ *  for example it will instantly set the state of the event to passed, by the database.
+ */
+async function add_timer_to_event(event_id, timetolive){
+    try{
+        const payload = {
+            _id: event_id,
+            timeToExecute: new Date(timetolive)
+        }
+        let events_clockwork = new EventClockStream(payload);
+        EventClockStream.collection.insertOne(payload);
+    }catch{
+        return false;
+    }
+    return true;
+}
+
+
+app.post("/event/get", async (req,res) => {
+    const check_query = [req.body.eventId]
+    for (const element of check_query){
+        if((typeof element != "string")){
+            return res.status(400).send({success: false, status: "invalid request object"});
+        }
+    }
+
+    let input = req.body;
+    console.log("/event/get: Request Body:", req.body);
+    let event_id = input.EventId;
+    
+    try{
+        EventOrchestrator.findOne({EventId: event_id}, (error, event) => {
+            if (error){
+                return res.status(500).send({success: false, status: "error", result: undefined});
+            }
+        // console.log(deviceData);
+            if(event == null){
+                return res.status(404).send({success: false, status: "not found", result: undefined});
+            }
+            return res.status(200).json({success: true, status: "found", result: event});
+        });
+    }catch{
+        return res.status(500).send({success: false, status: "error", result: undefined});
+    }
+});
+
+
+app.post("/event/delete", async (req,res) => {
+    const check_query = [req.body.eventId]
+    for (const element of check_query){
+        if((typeof element != "string")){
+            return res.status(400).send({success: false, status: "invalid request object"});
+        }
+    }
+    
+    let input = req.body;
+    console.log("/event/delete: Request Body:", req.body);
+    let event_id = input.eventId;
+    
+    try{
+        // Delete its event timer.
+        EventClockStream.deleteOne({_id: event_id});
+
+        EventOrchestrator.deleteOne({EventId: event_id}, (error, event) => {
+            if (error){
+                return res.status(500).send({success: false, status: "server error"});
+            }
+        // console.log(deviceData);
+            if(event == null){
+                return res.status(404).send({success: false, status: "not found"});
+            }
+            return res.status(200).json({success: true, status: "deleted"});
+        });
+
+
+    }catch{
+        return res.status(500).send({success: false, status: "server error"});
+    }
+});
+
+
+
+
+/*--------------------------------------------------DISPATCHER STATE REGION---------------------------------------------- */
+
+/*--------------------------------------------------DISPATCHER STATE REGION---------------------------------------------- */
+
+
+
+
+
+
+/*------------------------------------------------- ERROR HANDLING STATE REGION------------------------------------------ */
+/*------------------------------------------------- ERROR HANDLING STATE REGION------------------------------------------ */
+
+
+
+/*------------------------------------------------ EVENT TERMINATION STATE REGION--------------------------------------- */
+/*------------------------------------------------ EVENT TERMINATION STATE REGION--------------------------------------- */
+//Send to STATUS MICROSERVICE
+async function setEventLifeCycleComplete(event, status, messag, send_to_user){
+    const query = {EventId: eventid}
+    const updateExecutionDateFlag = {
+        $set: {
+            EventStatus: status,
+            ExecutionMessage: message,
+            SendToUser: send_to_user, 
+        },
+    }
+}
+
+
+/**
+ * 
+ *  eventid:
+ *   
+ *  returns
+ * {
+ *     status: true/false
+ *     predicateSatisfiedIncremented: true/false
+ *     message: ""
+ *      
+ * }
+ * 
+ */
+app.post("/event/satisfy_predicate", async (req,res) => {
+    const check_query = [req.body.eventId]
+    for (const element of check_query){
+        //shortcircuit
+        if((typeof element != "string")){
+            return res.status(400).send({success: false, status: "invalid response"});
+        }
+    }
+
+    let event_id = req.body.eventId;
+
+    //Locate the event field
+    let event_doc = await getEventState(event_id);
+
+    if(event_doc["SendToUser"] != false && event_doc["TotalPredicateSatisfied"] < event_doc["NumberOfPredicates"]){
+        const query = {EventId: eventid}
+        const updateExecutionDateFlag = {
+            $inc: {"TotalPredicateSatisfied": 1}
+        }
+        try{
+            await EventOrchestrator.updateOne(query,updateExecutionDateFlag);
+        }catch{
+            console.log("Critical Error has occured! Unable to increment PredicateSatisfyCount.");
+            console.log("EventID: ", event_id, " is now in an undefined state. -1 predicate");
+            console.log("SEND THIS TO STATUS REPORT MODULE INSTANTLY");
+        }
+    }
+});
 
 /**
  *  This is the event system: core of orchestra.
  *  This is a event based finite state system.
  *  Triggers are provided from 2 sources: Mongodb change stream[augmented by TTL], and 
  */
-
-
-
-app.post("/clockworks/add", async (req,res) => {
-    const check_query = [req.body.eventid]
+app.post("/debug/clockworks/add", async (req,res) => {
+    const check_query = [req.body.eventId]
     for (const element of check_query){
         if((typeof element != "string")){
             return res.status(400).send({success: false, status: "invalid response"});
         }
     }
 
-    let eventid = req.body.eventid;
+    let eventid = req.body.eventId;
     let timetolive = req.body.timetolive;
 
     if(!timetolive){
